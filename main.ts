73fb6129aeb68582e1830ab3652ace951c80262a
handsomeleeyishan <80392816+handsomeleeyishan@users.noreply.github.com>
2021-03-27 17:35:54 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -87,10 +87,5 @@ basic.forever(function () {
     }
 })
 control.inBackground(function () {
-    music.playTone(349, music.beat(BeatFraction.Double))
-    music.playTone(440, music.beat(BeatFraction.Double))
-    music.playTone(392, music.beat(BeatFraction.Double))
-    music.playTone(523, music.beat(BeatFraction.Double))
-    music.playTone(587, music.beat(BeatFraction.Breve))
-    music.playTone(587, music.beat(BeatFraction.Double))
+	
 })
